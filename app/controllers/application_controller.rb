6ae58cb45b4cd4a83cd856eff094efc3227927f7
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2018-02-02 23:34:43 -0500

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def hello
-    render html: '¡Hola, mundo!'
+    render html: 'Hello, ZA WARUDOOOOOOOO!'
   end
+
+  def hola
+      render html: '¡Hola, mundo!'
+    end
 
   def goodbye
     render html: 'Later, buddy'
